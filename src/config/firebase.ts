d3e4a9def6c4ea27a9dbe722788f9660cadccbbb
Abdullah1emad2farghaly <abdu123llahemad456@gmail.com>
2026-09-23 13:30:57 +0300

    added mobile scanner

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -23,11 +23,11 @@ import { getDatabase, type Database } from "firebase/database";
 import { getAuth, signInAnonymously, onAuthStateChanged, type Auth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
 };
 
 let app: FirebaseApp | null = null;
